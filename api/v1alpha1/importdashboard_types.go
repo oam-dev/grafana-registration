49d5c579df8dd1d0e312575466de5b580202e8a6
zzxwill <zzxwill@gmail.com>
2021-08-12 20:10:22 +0800

    make the controller

--- api/v1alpha1/importdashboard_types.go
+++ api/v1alpha1/importdashboard_types.go
@@ -23,7 +23,7 @@ import (
 // ImportDashboardSpec defines the desired state of ImportDashboard
 type ImportDashboardSpec struct {
 	Grafana Grafana  `json:"grafana"`
-	URLS     []string `json:"urls"`
+	URLS    []string `json:"urls"`
 }
 
 // ImportDashboardStatus defines the observed state of ImportDashboard
