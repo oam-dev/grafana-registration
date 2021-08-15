811e06544cbfd7d3b0e3c792398ee560dd6058d1
zzxwill <zzxwill@gmail.com>
2021-08-15 14:00:28 +0800

    update test data

--- controllers/grafana_test.go
+++ controllers/grafana_test.go
@@ -2,10 +2,10 @@ package controllers
 
 //func TestImportDashboard(t *testing.T) {
 //	ctx := context.Background()
-//	url := "https://raw.githubusercontent.com/zzxwill/grafana-dashboards/master/Kubernetes_for_Prometheus_Dashboard_CN_20201209_1628671317889.json"
+//	url := "https://raw.githubusercontent.com/zzxwill/grafana-dashboards/poc/kubevela_core_logging.json"
 //	board, _ := downloadGrafanaDashboard(url)
 //	grafanaURL := "http://grafana.c276f4dac730c47b8b8988905e3c68fcf.cn-hongkong.alicontainer.com/"
-//	basicAuth := "admin:vspgFXHScKDNr0sEdmOaLRGP7EKUsuEcCFCYraUi"
+//	basicAuth := "admin:gIi6CyVKoVlF5OxEGQyPfV7G0eqkGClgl8KE3nUt"
 //	err := importDashboard(ctx, grafanaURL, basicAuth, board)
 //	assert.NoError(t, err)
 //}
